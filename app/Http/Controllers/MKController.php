058472315dcaa507adaa9053c3417819fdbd386c
Yogikwrn <yogikbudiawan33@gmail.com>
2023-10-25 19:50:11 +0800

    TUGAS

--- app/Http/Controllers/MKController.php
+++ app/Http/Controllers/MKController.php
@@ -6,12 +6,40 @@ use Illuminate\Http\Request;
 
 class MKController extends Controller
 {
+    private $data = [
+        [   'ID MK' => "MK3712",
+            'NAMA MK' => "WEB PROGAMING",
+            'JURUSAN' => "SISTEM KOMPUTER",
+        ],
+        [
+            'ID MK' => "MK3099",
+            'NAMA MK' => "DATABASE",
+            'JURUSAN' => "SISTEM KOMPUTER",
+        ],
+        [   'ID MK' => "MK3023",
+            'NAMA MK' => "OPJECT ORIENTED PROGAMING",
+            'JURUSAN' => "SISTEM KOMPUTER",
+        ],
+        [
+            'ID MK' => "MK3033",
+            'NAMA MK' => "MICROPOSESOR",
+            'JURUSAN' => "SISTEM KOMPUTER",
+        ],
+    ];
+
     public function index()
     {
+        return view('MK.index', ['data' => $this->data]);
     }
 
     public function create()
     {
+        return view('MK.create');
+    }
+
+    public function edit($id)
+    {
+        return view('MK.edit', ['data' => $this->data[$id], 'id' => $id]);
     }
 
     public function show($id)
